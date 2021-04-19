197cc7799fdb8be5112b2b9eaad0d80ea2cede7e
evgen nazarenko <nazarenkoevgen99@gmail.com>
2021-04-19 19:31:50 +0300

    Practical-1

--- Practical-1/JS/script.js
+++ Practical-1/JS/script.js
@@ -1,41 +1,51 @@
 "use strict";
+
+console.log("-----------------  1  ----------------------");
+
 //1
 const Arr1 = [1, 5, "4", "hello"];
-console.log(Arr1[2]);
+console.log(Arr1[1]);
 
 const Arr2 = [true, 2, {}, ["a"], 222];
-console.log(Arr2[2]);
+console.log(Arr2[1]);
 
+console.log(Arr1[1] + Arr2[1]);
 //2
+console.log("-------------------  2  --------------------");
 
 Arr1[4] = 22;
 
 console.log(Arr1);
 
 //3
+console.log("-------------------  3  --------------------");
 
-for (const length of Arr2) {
-  console.log(length);
+for (const arr of Arr2) {
+  console.log(arr);
 }
 
 //4
+console.log("--------------------  4  --------------------");
 
 const message = "Welcome to Ukraine!";
 
-for (const character of message) {
-  console.log(character);
-}
+console.log(message.split(""));
 
 console.log(message.indexOf("l"));
+
 //5
+console.log("------------------------  5  -----------------------");
 
 const styles = ["Джаз", "Блюз"];
 
 styles.push("Рок-н-ролл");
+console.log(styles);
 
+const splice = styles.splice(1, 1, "Класика");
 console.log(styles);
 
 styles.shift("Джаз");
-styles.unshift("Реп", "Реггі");
+console.log(styles);
 
+styles.unshift("Реп", "Реггі");
 console.log(styles);
